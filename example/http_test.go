7c0d41a3c06ee7f6a7c813bb84f0ed8e2748525e
暗里着迷 <642028114@qq.com>
2023-11-29 09:55:58 +0800

    http封装post的表单请求方式

--- example/http_test.go
+++ example/http_test.go
@@ -3,6 +3,7 @@ package example
 import (
 	"fmt"
 	"github.com/ketianlin/ktools"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -37,4 +38,45 @@ func TestHttpPost(t *testing.T) {
 		return
 	}
 	fmt.Println(string(res))
+}
+
+func TestHttpFormPost(t *testing.T) {
+	url := "http://localhost:8080/submit"
+	headerEntity := map[string]string{
+		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+	}
+	res, err := ktools.Http.FormPost(url, map[string]interface{}{
+		"name":          "吊毛",
+		"email":         "diaomao@qq.com",
+		"status":        true,
+		"id":            666,
+		"register_time": time.Now().Format(time.RFC3339),
+	}, headerEntity)
+	if err != nil {
+		fmt.Println("http.Get error: ", err.Error())
+		return
+	}
+	fmt.Println(string(res))
+}
+
+func checkType2[T any](data T) {
+	switch reflect.TypeOf(data).Kind() {
+	case reflect.Struct:
+		fmt.Println("This is a struct")
+	case reflect.Map:
+		fmt.Println("This is a map")
+	default:
+		fmt.Println("Unknown type")
+	}
+}
+
+func checkType(data interface{}) {
+	switch reflect.TypeOf(data).Kind() {
+	case reflect.Struct:
+		fmt.Println("This is a struct")
+	case reflect.Map:
+		fmt.Println("This is a map")
+	default:
+		fmt.Println("Unknown type")
+	}
 }
--- http/http.go
+++ http/http.go
@@ -2,6 +2,7 @@ package httpT
 
 import (
 	"encoding/json"
+	structT "github.com/ketianlin/ktools/struct"
 	"github.com/valyala/fasthttp"
 )
 
@@ -28,7 +29,7 @@ func (e *Enter[T]) Get(url string, headerEntity ...map[string]string) ([]byte, e
 	return resp.Body(), nil
 }
 
-// Post 这里的contenttype只支持application/json, reqEntity这个可以是任意类型
+// Post 这里的contenttype只支持application/json, reqEntity这个可以是任意类型,reqEntity如果是结构体最好有设置json的tag
 func (e *Enter[T]) Post(url string, reqEntity T, headerEntity ...map[string]string) ([]byte, error) {
 	req := fasthttp.AcquireRequest()
 	resp := fasthttp.AcquireResponse()
@@ -52,5 +53,43 @@ func (e *Enter[T]) Post(url string, reqEntity T, headerEntity ...map[string]stri
 		return nil, err
 	}
 	//fmt.Println("result:\r\n", string(resp.Body()))
+	return resp.Body(), nil
+}
+
+// FormPost 这里的contenttype只支持application/x-www-form-urlencoded, reqEntity这个可以是任意类型,reqEntity如果是结构体最好有设置json的tag
+func (e *Enter[T]) FormPost(url string, reqEntity T, headerEntity ...map[string]string) ([]byte, error) {
+	// 创建一个 HTTP 请求
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+
+	// 设置请求 URL 和方法
+	req.SetRequestURI(url)
+	req.Header.SetMethod(fasthttp.MethodPost)
+	req.Header.SetContentType("application/x-www-form-urlencoded")
+
+	if len(headerEntity) > 0 {
+		for k, v := range headerEntity[0] {
+			req.Header.Add(k, v)
+		}
+	}
+
+	m := structT.Enter[any]{}.AnyToMap(reqEntity)
+	// 设置请求表单数据
+	args := req.PostArgs()
+	for k, v := range m {
+		args.Set(k, v)
+	}
+
+	// 发送请求并获取响应
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+	if err := fasthttp.Do(req, resp); err != nil {
+		return nil, err
+	}
+
+	// 输出响应结果
+	//fmt.Println(resp.StatusCode())
+	//fmt.Println(string(resp.Body()))
+
 	return resp.Body(), nil
 }
